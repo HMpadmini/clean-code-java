3621586e8b3bb540f094ed571a701aa9586d1b88
Padmini Hiriyur Manjunatha <padmini@Padminis-MBP.modem>
2022-11-11 15:47:30 +1100

    changed

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -17,4 +17,23 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+    private  double calculateAmount() {
+        double rentalPrice = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalPrice += 2;
+                if (getDaysRented() > 2)
+                    rentalPrice += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalPrice += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalPrice += 1.5;
+                if (getDaysRented() > 3)
+                    rentalPrice += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalPrice;
+    }
 }
--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -4,12 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerTest {
+
+
     @Test
     public void test() {
+        Customer customer = new Customer("Padmini");
+        Movie movie = new Movie("Avatar", 2);
+        Rental rental = new Rental(movie, 1);
+        System.out.println(customer.statement());
+        String expected = "Rental Record for Padmini\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter points";
 
-        int expected = 3;
-
-        Assert.assertEquals(expected, 1 + 2);
+        Assert.assertEquals(expected, "Rental Record for Padmini\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter points");
     }
 
 }
